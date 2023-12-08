2feaf256985e7b1d779ff00053c26743ccca7792
tv <tv@localhost.localdomain>
2023-12-08 18:28:45 +0200

    Improved error handling and script optimization
    - Enhanced error handling with try-catch blocks.
    - Optimized checks for NuGet and PSWindowsUpdate installations.

--- updeitti.ps1
+++ updeitti.ps1
@@ -1,35 +1,40 @@
-# Aseta suoritusoikeudet sekä asenna nuget
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-Install-PackageProvider -Name NuGet -Force
-Import-PackageProvider -Name NuGet
 
+# Tarkista, onko NuGet jo asennettu ja asenna se tarvittaessa ilman vahvistusta
+if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
+    Write-Output "Asennetaan NuGet..."
+    Install-PackageProvider -Name NuGet -ForceBootstrap -Force
+    Import-PackageProvider -Name NuGet
+}
 
-# Päivitä PSGallery : PSRepository
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Get-PSRepository -Name PSGallery | Format-List * -Force
+# Tarkista, onko PSWindowsUpdate-moduuli asennettu
+$psWindowsUpdateModule = Get-Module -ListAvailable -Name PSWindowsUpdate
+if (-not $psWindowsUpdateModule) {
+    Write-Output "PSWindowsUpdate-moduulia ei löydy. Asennetaan moduuli..."
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Get-PSRepository -Name PSGallery | Format-List * -Force
+    Install-Module -Name PSWindowsUpdate -Force -Confirm:$false
+}
 
-# Listaa moduulit
-Write-Output "Running:  Get-InstalledModule"
-Get-InstalledModule
-
-# Asenna tarvittavat moduulit
-Write-Output "Running:  Install-Module -Name PSWindowsUpdate -Force"
-Install-Module -Name PSWindowsUpdate -Force
-
-# Tuo moduulit
+# Tuo PSWindowsUpdate-moduuli
 Import-Module -Name PSWindowsUpdate
 
-# Listaa moduulien komennot:
-Get-Command -Module PSWindowsUpdate
-
-# Tarkista onko Microsoft Update palvelu saatavilla.
-# Lisää update palvelu jollei.
+# Tarkista onko Microsoft Update palvelu saatavilla ja lisää se tarvittaessa
 $MicrosoftUpdateServiceId = "7971f918-a847-4430-9279-4a52d1efe18d"
-If ((Get-WUServiceManager -ServiceID $MicrosoftUpdateServiceId).ServiceID -eq $MicrosoftUpdateServiceId) { Write-Output "Confirm that Microsoft Update Service is registered ...." }
-Else { Add-WUServiceManager -ServiceID $MicrosoftUpdateServiceId -ErrorAction SilentlyContinue -Confirm:$false }
-# Now, check again to ensure it is available.  If not -- fail the script:
-If (!((Get-WUServiceManager -ServiceID $MicrosoftUpdateServiceId).ServiceID -eq $MicrosoftUpdateServiceId)) { Throw "ERROR:  Microsoft Update Service is not registered :( " }
+if ((Get-WUServiceManager -ServiceID $MicrosoftUpdateServiceId).ServiceID -ne $MicrosoftUpdateServiceId) {
+    Write-Output "Microsoft Update Service ei ole rekisteröity. Lisätään palvelu..."
+    Add-WUServiceManager -ServiceID $MicrosoftUpdateServiceId -ErrorAction SilentlyContinue -Confirm:$false
+}
 
-# Pakota asennus ja käynnistä uudelleen
-Get-WUInstall -MicrosoftUpdate -AcceptAll -AutoReboot 
-Get-WUInstall -MicrosoftUpdate -AcceptAll -Download -Install -AutoReboot
+# Varmista, että Microsoft Update Service on rekisteröity
+if (!((Get-WUServiceManager -ServiceID $MicrosoftUpdateServiceId).ServiceID -eq $MicrosoftUpdateServiceId)) {
+    Throw "Virhe: Microsoft Update Service ei ole rekisteröity. Päivitystä ei voida suorittaa."
+}
+
+try {
+    Write-Output "Aloitetaan Windows Update..."
+    Get-WUInstall -MicrosoftUpdate -AcceptAll -AutoReboot -ErrorAction Stop
+    Get-WUInstall -MicrosoftUpdate -AcceptAll -Download -Install -AutoReboot -ErrorAction Stop
+} catch {
+    Write-Error "Windows Update -prosessissa tapahtui virhe: $_. Tarkista yhteys ja yritä uudelleen."
+}
